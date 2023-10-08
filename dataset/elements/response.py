8d5171bb4de66259d7075d13946c43add5ad908e
Byte Bender <136265142+BYT-Bender@users.noreply.github.com>
2023-10-08 21:49:48 +0530

    Update response.py

--- dataset/elements/response.py
+++ dataset/elements/response.py
@@ -1,64 +1,63 @@
 import json
+import os
 
-def get_element_info(data, field, value):
-    for element in data["elements"]:
-        if element[field] == value:
-            return element
-    return None
+class SearchElement:
+    def __init__(self):
+        self.element_data = self.load_json("elements.json")
+        self.property_intents = self.load_json("intents.json")
+        self.elements = [element['name'] for element in self.element_data['elements']]
+        self.property_patterns = {element['tag']: element['patterns'] for element in self.property_intents['intents']}
+        self.question_strings = ["what is", "tell me", "do you know", "arre you aware of"]
 
-def element_search(element_name):
-    element_name = element_name.lower().capitalize()
+    def load_json(self, filename):
+        file_path = os.path.join("D:\Files\Python\AI\CURRENT_VER\dataset\elements", filename)
+        with open(file_path, "r") as file:
+            return json.load(file)
 
-    with open("D:\Files\Python\AI\CURRENT_VER\dataset\elements\elements.json", "r") as elements_dataset:
-        elements_data = json.load(elements_dataset)
+    def find_element(self, user_message):
+        for element in self.elements:
+            if element.lower() in user_message.lower():
+                return element
 
-    elements_info = get_element_info(elements_data, "name", element_name)
+    def find_property_tag(self, user_message):
+        for tag, patterns in self.property_patterns.items():
+            for prop in patterns:
+                if prop.lower() in user_message.lower():
+                    return tag
 
-    if elements_info:
-        description = elements_info["description"].capitalize()
-        symbol = elements_info["symbol"]
-        atomic_number = elements_info["atomic_number"]
-        atomic_mass = elements_info["atomic_mass"]
-        electron_configuration = elements_info["electron_configuration"]
-        appearance = elements_info["appearance"]
-        density = elements_info["density"]
-        boiling_point = elements_info["boiling_point"]
-        melting_point = elements_info["melting_point"]
-        atomic_radius = elements_info["atomic_radius"]
-        electronegativity = elements_info["electronegativity"]
-        ionization_energy = elements_info["ionization_energy"]
-        oxidation_states = elements_info["oxidation_states"]
+    def get_element_info(self, data, parent_field, field, value):
+        for element in data[parent_field]:
+            if element[field] == value:
+                return element
+        return None
 
-        discovered_by = elements_info["discovered_by"]
-        discovery_year = elements_info["discovery_year"]
+    def find_property_value(self, element, property_tag):
+        property_value = self.get_element_info(self.element_data, "elements", "name", element.capitalize())
+        return property_value[property_tag]
 
-        if discovery_year == "Ancient":
-            discovery_str = f'{discovered_by}'
-        else:
-            discovery_str = f'{discovered_by} in {discovery_year}'
-
-        # response = f'{description} Its chemical symbol is "{symbol}", and it has an atomic number of {atomic_number}. Discovered by {discovered_by}{discovery_str}.'
-        
-        description = elements_info["description"]
-        response = f'''{description[:len(element_name)]} (symbol: {symbol}, atomic number: {atomic_number}) {description[len(element_name)+1:]}
-Here are some essential details about hydrogen:
-- Atomic Mass: {element_name} has an atomic mass of approximately {atomic_mass} atomic mass units (u).
-- Electron Configuration: {electron_configuration}.
-- Discovery: Hydrogen was discovered by the British scientist {discovery_str}.
-- Appearance: {element_name} is commonly found as a {appearance}.
-- Density: Its density is approximately {density:f} grams per cubic centimeter.
-- Boiling Point: {element_name} boils at approximately {boiling_point} degrees Celsius.
-- Melting Point: Its melting point is about {melting_point} degrees Celsius.
-- Atomic Radius: The atomic radius of {element_name} is {atomic_radius} picometers (pm).
-- Electronegativity: {element_name} has an electronegativity value of {electronegativity} on the Pauling scale.
-- Ionization Energy: The ionization energy of {element_name}, the energy required to remove an electron from a hydrogen atom, is {ionization_energy} kJ/mol.
-- Oxidation States: {element_name} can exist in oxidation states {oxidation_states}.'''
-
-        # response = f'{element_name} (symbol: {symbol}, atomic number: {atomic_number}) is the lightest and most abundant element in the universe. It is a fundamental building block of matter and plays a crucial role in various natural processes and human activities. Here are some essential details about hydrogen:'
-
-
+    def get_responses(self, property_tag, element, property_value):
+        prop_data = self.get_element_info(self.property_intents, "intents", "tag", property_tag)
+        response = prop_data['response'].format(element=element, property=property_value)
         return response
-    else:
-        return f"{element_name} not found in the dataset."
 
-print(element_search("hydrogen"))
+    def generate_response(self, user_message):
+        found_question = any(question.lower() in user_message.lower() for question in self.question_strings)
+
+        if found_question:
+            element = self.find_element(user_message)
+            property_tag = self.find_property_tag(user_message)
+
+            if element and property_tag:
+                property_value = self.find_property_value(element, property_tag)
+                response = self.get_responses(property_tag, element, property_value)
+                return response
+            else:
+                return None
+        else:
+            return None
+
+    def main(self):
+        pass
+
+if __name__ == "__main__":
+    SearchElement().main()
