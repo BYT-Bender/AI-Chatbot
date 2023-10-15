f85b1fc0a3a8a46a48b256702d1174a1f1cdab6c
Byte Bender <136265142+BYT-Bender@users.noreply.github.com>
2023-10-15 09:49:35 +0530

    Update analize.py

--- analize.py
+++ analize.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-response_df = pd.read_csv("data/response_data.csv")
+# Response by ID
+response_df = pd.read_csv("data/response_usage.csv")
 
 plt.bar(response_df["id"], response_df["count"], color='b')
 plt.xticks(response_df["id"])
 plt.xlabel("Response ID")
 plt.ylabel("Response Count")
 plt.title("Response Distribution by ID")
+
+plt.show()
+
+
+# Command Usage by ID
+command_df = pd.read_csv("data/command_usage.csv")
+
+plt.bar(command_df["id"], command_df["count"], color='b')
+plt.xticks(command_df["id"])
+plt.xlabel("Command ID")
+plt.ylabel("Command Count")
+plt.title("Command Distribution by ID")
 
 plt.show()
